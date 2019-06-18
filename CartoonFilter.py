c6df5b268e73484b2c15c1feb183069f91efdc99
Abner Santos <abner.santos@usp.br>
2019-06-18 15:00:04 -0300

    Added edge detection and some sample images

--- CartoonFilter.py
+++ CartoonFilter.py
@@ -8,6 +8,19 @@ d = 20
 sigma_color = 9
 sigma_space = 7
 
+'''
+#   Gaussian Filter
+#   Degradation function used for the image deblurring
+#       k: Filter size
+#       sigma: Equation parameter/intensity
+#       return: Degradation filter
+'''
+def gaussian_filter (k=3, sigma=1.0):
+    arx = np.arange((-k // 2) + 1.0 , (k // 2) + 1.0)
+    x , y = np.meshgrid(arx , arx)
+    filt = np.exp(-(1/2)*(np.square(x) + np.square(y)) / np.square(sigma))
+    return filt / np.sum(filt)
+
 # Reads the image
 filename = str(input()).rstrip() 
 
@@ -15,7 +28,6 @@ image = cv2.imread(filename)
 
 # Creates a copy of the image
 image_cartoon = np.array(image, copy=True)
-image_edges = np.array(image, copy=True)
 
 # Repeatedly applies bilateral filtering to each color channel
 for _ in range(num_bilateral):
@@ -39,13 +51,16 @@ cv2.imshow("cartoon", image_cartoon)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+# Creates a gray copy of the cartoon image
+image_edges = np.array(image_cartoon, copy=True)
+image_edges = cv2.cvtColor(image_edges, cv2.COLOR_BGR2GRAY)
+
 # Mask for the edge detection
 kernel = np.array([[-1, -1, -1], [-1,  8, -1], [-1, -1, -1]])
+image_edges = cv2.filter2D(image_edges, -1, gaussian_filter())
 
 # Applies the convolution
-image_edges[:,:,0] = cv2.filter2D(image_edges[:,:,0], -1, kernel)
-image_edges[:,:,1] = cv2.filter2D(image_edges[:,:,1], -1, kernel)
-image_edges[:,:,2] = cv2.filter2D(image_edges[:,:,2], -1, kernel)
+image_edges = cv2.filter2D(image_edges, -1, kernel)
 
 # Displays the unfiltered result of the edge detection
 cv2.imshow("edges", image_edges)
@@ -55,15 +70,26 @@ cv2.destroyAllWindows()
 # Applies threshold
 for x in range(image_edges.shape[0]):
     for y in range(image_edges.shape[1]):
-        if np.sum(image_edges[x][y]) > 150:
-            image_edges[x][y] = (255, 255, 255)
+        if np.sum(image_edges[x][y]) > 15:
+            image_edges[x][y] = 255
         else:
-            image_edges[x][y] = (0, 0, 0)
+            image_edges[x][y] = 0
 
-# Displays the edges after the threshold as white outlines
-cv2.imshow("edges", image_edges)
+# Displays the edges after the threshold
+cv2.imshow("edges with threshold", image_edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 new_filename = os.path.splitext(filename)[0] + "_cartoonized.png"
 cv2.imwrite(new_filename, image_cartoon)
+
+# Applies threshold
+for x in range(image_edges.shape[0]):
+    for y in range(image_edges.shape[1]):
+        if np.sum(image_edges[x][y]) == 255:
+            image_cartoon[x][y] = (255, 255, 255)
+
+# Displays the edges after the threshold
+cv2.imshow("Image with edges", image_cartoon)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
